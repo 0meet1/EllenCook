78265e5d315f1f48f1f93abeb06932a055238360
邹海清 <zouhaiqing@zouhaiqingdemacbook-pro.local>
2016-02-24 15:15:26 +0800

    Initial Commit

---
+++ EllenCook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EllenCook
+//
+//  Created by 邹海清 on 16/2/24.
+//  Copyright © 2016年 ellen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EllenCook/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EllenCook
+//
+//  Created by 邹海清 on 16/2/24.
+//  Copyright © 2016年 ellen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
